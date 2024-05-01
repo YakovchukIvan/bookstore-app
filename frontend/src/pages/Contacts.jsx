68e2b22df3f5a1d90b20a168d6ac78dfac4089be
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2024-05-01 22:06:21 +0300

    The logic of sorting by popularity, title and date has been implemented

--- frontend/src/pages/Contacts.jsx
+++ frontend/src/pages/Contacts.jsx
@@ -17,10 +17,28 @@ function Contacts() {
   }, [booksData]);
 
   const sortBooks = (sort) => {
-    console.log(sort);
-    const test = [...books].sort((a, b) => b.rating - a.rating);
-    console.log(test);
-    setBooks(test);
+    let sortedBooks = [...books]; // Клонуємо масив книг для безпечного сортування
+
+    switch (sort) {
+      case SORT_KEYS[0]:
+        sortedBooks.sort((a, b) => b.rating - a.rating);
+        break;
+      case SORT_KEYS[1]:
+        const collator = new Intl.Collator('uk-UA', { sensitivity: 'base' });
+        sortedBooks.sort((a, b) => collator.compare(a.title, b.title));
+        break;
+      case SORT_KEYS[2]:
+        sortedBooks.sort(
+          (a, b) => new Date(a.dateOfEntry) - new Date(b.dateOfEntry)
+        );
+        break;
+      default:
+        // Якщо передано невірне значення для сортування, залишити незмінним
+        break;
+    }
+
+    // Оновлення стану з відсортованим масивом книг
+    setBooks(sortedBooks);
   };
 
   return (
