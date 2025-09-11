5ba951f73476a0dac90e81ee200b3bf4a41524ed
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2025-09-11 20:51:16 +0300

    Add eslint and TS config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,21 +1,58 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+// eslint.config.cjs
+const js = require('@eslint/js');
+const globals = require('globals');
+const react = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+const reactRefresh = require('eslint-plugin-react-refresh');
+const prettier = require('eslint-plugin-prettier');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    ignores: ['dist'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'func-style': ['error', 'expression'],
+      'prefer-arrow-callback': ['error'],
+      'arrow-body-style': ['error', 'as-needed'],
+      complexity: ['error', 5],
+      curly: ['error', 'multi-line'],
+      'max-lines': ['error', 255],
+      'react/display-name': 'off',
+      'react/jsx-fragments': ['warn', 'element'],
+      'react/prop-types': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      'react-hooks/rules-of-hooks': 'warn',
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    extends: [
+      js.configs.recommended,
+      'plugin:@typescript-eslint/recommended',
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      'plugin:prettier/recommended',
     ],
   },
-}
+];
