1fcc3f734530835b8e569f8e424693791e7562e3
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2025-09-22 21:17:16 +0300

    layout

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -15,14 +15,12 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<MainLayouts />}>
-            {/* <div className="container"> */}
             <Route index element={<Home />}></Route>
             <Route path="books" element={<BookStore />}></Route>
             <Route path="books/:slug" element={<SingleCourse />}></Route>
             <Route path="about" element={<About />}></Route>
             <Route path="contacts" element={<Contacts />}></Route>
             <Route path="*" element={<NotFound />}></Route>
-            {/* </div> */}
           </Route>
         </Routes>
       </div>
